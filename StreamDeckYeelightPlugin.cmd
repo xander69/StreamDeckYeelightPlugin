393fdfde239b0e04bcc52f2392eb6a2abe5d44d7
Alexander Shakhov <xander.shahov@gmail.com>
2022-11-18 01:56:19 +0300

    created empty plugin project

---
+++ StreamDeckYeelightPlugin.cmd
@@ -0,0 +1,2 @@
+﻿cd %appdata%\Elgato\StreamDeck\Plugins\com.xander.yeelight
+StreamDeckYeelightPlugin.exe -break %*
---
+++ models/CounterSettingsModel.cs
@@ -0,0 +1,7 @@
+﻿namespace StreamDeckYeelightPlugin.Models
+{
+    public class CounterSettingsModel
+    {
+        public int Counter { get; set; } = 0;
+    }
+}
